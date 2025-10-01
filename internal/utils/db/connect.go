485238abd57d50ff7259370dce1f27f8d6f691d6
DogukanGun <dogukangundogan5@gmail.com>
2025-10-01 11:34:03 +0200

    fix: add new session endpoint and kanalabs mcp server

--- internal/utils/db/connect.go
+++ internal/utils/db/connect.go
@@ -1,11 +1,38 @@
 package db
 
 import (
+	"context"
+	"crypto/tls"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"time"
 )
 
 func ConnectToDB(connectionUri string) (*mongo.Client, error) {
-	client, _ := mongo.Connect(options.Client().ApplyURI(connectionUri))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Configure TLS for MongoDB Atlas
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: false,
+	}
+
+	clientOptions := options.Client().
+		ApplyURI(connectionUri).
+		SetTLSConfig(tlsConfig).
+		SetServerSelectionTimeout(10 * time.Second).
+		SetConnectTimeout(10 * time.Second)
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Test the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
 	return client, nil
 }
